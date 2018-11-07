685aa32ae4ac5ad51be80573edef43c881820753
Francesco Longoni <fra-long96@hotmai.it>
2018-11-07 19:33:58 +0100

    non funziona quello che volevo fare...

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
-function preload(){
-  // put preload code here
-}
+var mic;
+var capture;
+var col = 400;
+var rig = 300;
 
+function preload() {
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  capture = createCapture(VIDEO);
+  capture.size(col, rig);
+  capture.hide();
+
+}
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight)
+    noStroke();
+    pixelDensity(1);
+    imageMode(CENTER);
 }
 
 function draw() {
-  // put drawing code here
+  translate(width/2, height/2);
+  var vol = mic.getLevel()*1000;
+  background(200);
+  var myImage = capture.loadPixels();
+  image(myImage, 0, 0, vol+100, vol+100);
+
+  // ellipse(250,250,vol * 200);
+
+
+
 }
